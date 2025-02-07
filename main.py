aeeab80f41341cac2597d349a77ada4c59fc4508
noumanahaq <noumanahaq@gmail.com>
2025-02-07 18:43:06 +0000

    Updating main.py file

---
+++ main.py
@@ -0,0 +1,23 @@
+# My First Calculator App
+
+# Main class
+class myCalculator(self):
+
+    # Addition
+    def add(self, x, y):
+        return x + y
+    
+    # Subtraction
+    def subtract(self, x, y):
+        return x - y
+
+    # Multiplication
+    def multiply(self, x, y):
+        return x * y
+    
+    # Division
+    def divide(self, x, y):
+        # Zero division error
+        if y == 0:
+            return "Error! Division by zero."
+        return x / y
